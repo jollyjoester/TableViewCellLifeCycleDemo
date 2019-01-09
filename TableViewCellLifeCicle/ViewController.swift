1962bb017a342aa53cf84dc3381c1f1191258291
jollyjoester <jollyjoester@gmail.com>
2019-01-09 12:37:44 +0900

    Init project.

---
+++ TableViewCellLifeCicle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewCellLifeCicle
+//
+//  Created by jollyjoester on 2019/01/09.
+//  Copyright © 2019 jollyjoester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
