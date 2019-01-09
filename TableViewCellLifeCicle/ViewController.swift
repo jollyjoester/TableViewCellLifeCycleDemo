189c29fbe187cc311c1aec04a11c254820bc9d4a
jollyjoester <jollyjoester@gmail.com>
2019-01-09 14:16:07 +0900

    Imple TabelView

--- TableViewCellLifeCicle/ViewController.swift
+++ TableViewCellLifeCicle/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let sampleData = [
+        "test1",
+        "test2",
+        "test3",
+        "test4",
+        "test5",
+        "test6",
+        "test7",
+        "test8",
+        "test9",
+        "test10",
+        "test11",
+        "test12",
+        "test13",
+        "test14",
+        "test15",
+        "test16",
+        "test17",
+        "test18",
+        "test19",
+        "test20"
+    ]
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return sampleData.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            fatalError()
+        }
+        
+        print(sampleData[indexPath.row])
+        
+        cell.textLabel?.text = sampleData[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+}
